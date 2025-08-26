0ef5bee08f292b40f77ff15fed3e235f70aa57a1
Joaquin12DM <joaquindm1226@gmail.com>
2025-08-26 04:31:58 -0500

    Listar Incidencias y buscar por Id

---
+++ GestionIncidencia-Backend/Controllers/IncidenciaController.cs
@@ -0,0 +1,36 @@
+﻿using GestionIncidencia.Application.Mapper;
+using Incidencias.Aplicacion.Service;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GestionIncidencia_Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class IncidenciaController : ControllerBase
+    {
+
+        private readonly IncidenciaService incidenciaService;
+
+        public IncidenciaController(IncidenciaService incidenciaService)
+        {
+            this.incidenciaService = incidenciaService;
+        }
+
+        [HttpGet("listaIncidencias")]
+        public async Task<IActionResult> ListaIncidencias()
+        {
+            var incidencias = await incidenciaService.ListarAsync();
+            return Ok(IncidenciaMapper.ToResponseList(incidencias));
+        }
+
+        [HttpGet("findById")]
+        public async Task<IActionResult> IncidenciaById(int id)
+        {
+            var incidencia = await incidenciaService.ObtenerPorIdAsync(id);
+            return Ok(IncidenciaMapper.ToResponse(incidencia));
+        }
+
+
+    }
+}
---
+++ GestionIncidencia.Application/DTO/Response/IncidenciaResponse.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionIncidencia.Application.DTO.Response
+{
+    public class IncidenciaResponse
+    {
+        public int IdIncidencia { get; set; }
+        public string Titulo { get; set; }
+        public string Descripcion { get; set; }
+        public string Estado { get; set; }
+        public string Prioridad { get; set; }
+        public string Tipo { get; set; }
+        public DateTime FechaCreacion { get; set; }
+        public DateTime FechaActualizacion { get; set; }
+    }
+}
